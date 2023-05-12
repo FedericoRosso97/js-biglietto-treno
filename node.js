b3fd3249043cf4a7afe2c9f8ed850300b625d0d1
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-12 17:19:20 +0200

    ottimizzazione

--- node.js
+++ node.js
@@ -9,11 +9,11 @@ console.log(etaUtente);
 if(etaUtente<18){
     let sconto=((20/100)*pLordo);
     let prezzoDec=(pLordo-sconto);
-    let prezzo=prezzoDec.toFixed(2);
+     prezzo=prezzoDec.toFixed(2);
 }else if(etaUtente>65){
     let sconto=((40/100)*pLordo);
     let prezzoDec=(pLordo-sconto);
-    let prezzo=prezzoDec.toFixed(2);
+     prezzo=prezzoDec.toFixed(2);
 }else{
     let prezzo=pLordo;
 }

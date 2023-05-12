986edeaef5b40802ee39dfcddda7314db2e55fea
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-12 16:11:01 +0200

    creo sconto per over 65 e arrotondo 2 decimami

--- node.js
+++ node.js
@@ -6,8 +6,16 @@ console.log(pLordo);
 const etaUtente=parseInt(prompt('quanti anni hai?'));
 console.log(etaUtente);
 
+document.getElementById('prezzo').innerHTML=pLordo;
 if(etaUtente<18){
-    let prezzo=((20/100)*pLordo);
+    let sconto=((20/100)*pLordo);
+    let prezzoDec=(pLordo-sconto);
+    let prezzo=prezzoDec.toFixed(2);
+    document.getElementById('prezzo').innerHTML=prezzo;
+}else if(etaUtente>65){
+    let sconto=((40/100)*pLordo);
+    let prezzoDec=(pLordo-sconto);
+    let prezzo=prezzoDec.toFixed(2);
     document.getElementById('prezzo').innerHTML=prezzo;
 }
 

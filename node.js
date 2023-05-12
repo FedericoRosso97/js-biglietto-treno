06e3318b049649831765798ffba26005e8b0d598
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-12 15:38:09 +0200

    creo if per calcolare sconto per minorenni

--- node.js
+++ node.js
@@ -1,4 +1,16 @@
 
 const userkilometers=parseInt(prompt('quanti km vuoi percorrere?')); 
+let pLordo=(userkilometers*0.21)
+console.log(pLordo);
+
 const etaUtente=parseInt(prompt('quanti anni hai?'));
+console.log(etaUtente);
+
+if(etaUtente<18){
+    let prezzo=((20/100)*pLordo);
+    document.getElementById('prezzo').innerHTML=prezzo;
+}
+
+
+
 

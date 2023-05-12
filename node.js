09fe5f3e38b34a5087d31dbc3fcdb41100064d85
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-12 16:55:20 +0200

    try ottimizzazione

--- node.js
+++ node.js
@@ -1,23 +1,24 @@
 
 const userkilometers=parseInt(prompt('quanti km vuoi percorrere?')); 
 let pLordo=(userkilometers*0.21)
-console.log(pLordo);
+console.log(prezzo);
 
 const etaUtente=parseInt(prompt('quanti anni hai?'));
 console.log(etaUtente);
-
-document.getElementById('prezzo').innerHTML=pLordo;
 if(etaUtente<18){
     let sconto=((20/100)*pLordo);
     let prezzoDec=(pLordo-sconto);
     let prezzo=prezzoDec.toFixed(2);
-    document.getElementById('prezzo').innerHTML=prezzo;
 }else if(etaUtente>65){
     let sconto=((40/100)*pLordo);
     let prezzoDec=(pLordo-sconto);
     let prezzo=prezzoDec.toFixed(2);
-    document.getElementById('prezzo').innerHTML=prezzo;
+}else{
+    let prezzo=pLordo;
 }
+document.getElementById.innerHTML=prezzo;
+
+
 
 
 

5112de486ac100873aa70c5414334b2b0f725b4b
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-15 14:30:50 +0200

    correzione/ottimizzazione codice

--- node.js
+++ node.js
@@ -4,20 +4,19 @@ let pLordo=(userkilometers*0.21)
 console.log(pLordo);
 
 let prezzo=pLordo;
+
 const etaUtente=parseInt(prompt('quanti anni hai?'));
 console.log(etaUtente);
+
 if(etaUtente<18){
     let sconto=((20/100)*pLordo);
-    let prezzoDec=(pLordo-sconto);
-     prezzo=prezzoDec.toFixed(2);
+     prezzo=(pLordo-sconto);
+     
 }else if(etaUtente>65){
     let sconto=((40/100)*pLordo);
-    let prezzoDec=(pLordo-sconto);
-     prezzo=prezzoDec.toFixed(2);
-}else{
-    let prezzo=pLordo;
+     prezzo=(pLordo-sconto);
 }
-document.getElementById("costo").innerHTML=prezzo;
+document.getElementById("costo").innerHTML=prezzo.toFixed(2)+'$';
 
 
 
